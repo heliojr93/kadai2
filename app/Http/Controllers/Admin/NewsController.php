35ca4080ab43d9ed7667829cb125952ba0485f61
名前 <>
2020-07-28 05:03:55 +0000

    kadai11

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 //以下を追加
 use App\News;
+// 以下を追記
+use Carbon\Carbon;
 
 class NewsController extends Controller
 {
@@ -91,6 +93,12 @@ class NewsController extends Controller
 
       // 該当するデータを上書きして保存する
       $news->fill($news_form)->save();
+      
+      // 以下を追記
+        $history = new History;
+        $history->news_id = $news->id;
+        $history->edited_at = Carbon::now();
+        $history->save();
 
       return redirect('admin/news');
   }
@@ -99,6 +107,6 @@ class NewsController extends Controller
       $news = News::find($request->id);
       // 削除する
       $news->delete();
-      return redirect('admin/news/');
+      return redirect('admin/news');
   }
 }
